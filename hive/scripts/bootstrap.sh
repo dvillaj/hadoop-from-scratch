95253963307f08ffd6a5f02a8038397660001ad4
Daniel Villanueva <dvillaj@gmail.com>
2020-05-27 19:23:00 +0200

    Hive wailt for mysql

--- hive/scripts/bootstrap.sh
+++ hive/scripts/bootstrap.sh
@@ -4,7 +4,7 @@ rm /tmp/*.pid
 service ssh start
 
 # init hive metastorage
-$HIVE_HOME/bin/schematool -dbType derby -initSchema
+wait-for-it.sh mysql:3306 -t 100 -- $HIVE_HOME/bin/schematool -dbType mysql -initSchema
 
 $HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/hive/warehouse
 $HADOOP_HOME/bin/hdfs dfs -chmod g+w /user/hive/warehouse
@@ -13,5 +13,7 @@ $HADOOP_HOME/bin/hdfs dfs -chmod g+w /user/hive/warehouse
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
 fi
+
+
 
 bash

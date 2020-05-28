8228ce5e8f18f01ca854cf389e907b0d50c3cdca
Daniel Villanueva <dvillaj@gmail.com>
2020-05-28 07:44:55 +0200

    Hive ok

--- hive/scripts/bootstrap.sh
+++ hive/scripts/bootstrap.sh
@@ -9,6 +9,9 @@ wait-for-it.sh mysql:3306 -t 100 -- $HIVE_HOME/bin/schematool -dbType mysql -ini
 $HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/hive/warehouse
 $HADOOP_HOME/bin/hdfs dfs -chmod g+w /user/hive/warehouse
 
+mkdir -p /usr/local/hive/logs
+hive --service hiveserver2  > /usr/local/hive/logs/hive2.log 2>&1 &
+hive --service metastore  > /usr/local/hive/logs/metastore.log 2>&1 &
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
